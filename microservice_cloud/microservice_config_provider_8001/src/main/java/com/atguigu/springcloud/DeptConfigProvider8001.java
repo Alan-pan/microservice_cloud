5ec73c12b0abeb6404c734cbedb992e4a980f817
Alan-pan <294332049@qq.com>
2019-11-17 17:41:39 +0800

    spring config配置。。。

---
+++ microservice_cloud/microservice_config_provider_8001/src/main/java/com/atguigu/springcloud/DeptConfigProvider8001.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableDiscoveryClient//服务发现
+@SpringBootApplication
+public class DeptConfigProvider8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConfigProvider8001.class,args);
+    }
+}
