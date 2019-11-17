fe6fb3ded85fe75a378227bb45f44f27033410ac
Alan-pan <294332049@qq.com>
2019-11-17 08:13:13 +0800

    新增了zuul网关配置

---
+++ microservice_cloud/microservice_zuul_gateway_9527/src/main/java/com/atguigu/springcloud/ZuulGateWay9527.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGateWay9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGateWay9527.class,args);
+    }
+}
